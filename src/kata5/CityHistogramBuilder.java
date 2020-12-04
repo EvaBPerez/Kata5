1673f727fdc48c608dc2cb1ed465b105967b1404
Eva <Eva@PC-de-Eva>
2020-12-04 14:07:36 +0000

    KATA5, MAIL HISTOGRAM

---
+++ src/kata5/CityHistogramBuilder.java
@@ -0,0 +1,16 @@
+package kata5;
+
+import java.util.List;
+
+public class CityHistogramBuilder {
+    public static Histogram<String> build(List<Mail> mailList) {
+        Histogram<String> histogram = new Histogram<>();
+        
+        for (Mail mail : mailList) {
+            histogram.increment(mail.getDomain());
+        }
+        
+        return histogram;
+        
+    }
+}
